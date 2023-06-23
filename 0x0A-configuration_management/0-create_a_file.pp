55a0b6436b2684e90bb3c5e61b8362b0c88226cc
Derrick-ndemo <derrickndemo@gmail.com>
2023-06-24 01:23:43 +0300

    create a file using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file with content, mode, owner and group
+
+file { '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
