7ae3fd23402adff711062929548d7de0ca5d455a
Derrick-ndemo <derrickndemo@gmail.com>
2023-06-24 01:49:42 +0300

    Updated arrow alignment

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # create a manifest that kills process named killmenow
 
 exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => '/bin/',
+  command => 'pkill -f killmenow',
+  path    => '/bin/',
 }
